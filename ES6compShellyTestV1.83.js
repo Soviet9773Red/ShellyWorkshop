096a67d2c166e014044e0783dfca733c4e3b17b5
Alexander <agabrielsten@gmail.com>
2026-01-02 14:22:16 +0100

    Update ES6compShellyTestV1.83.js

--- ES6compShellyTestV1.83.js
+++ ES6compShellyTestV1.83.js
@@ -1,3 +1,10 @@
+/*
+ * ES6compShellyTest v1.83
+ * (c) 2025 Alexander Soviet9773Red | https://github.com/Soviet9773Red
+ * Developed during Shelly Scripting Advanced – October 2025
+ * MIT License
+*/
+
 // Shelly Compatibility Test + Auto Device Info + Endpoint URLs
 // Safe v1.83 – adds Shelly.GetDeviceInfo() to show firmware and device id
 
@@ -294,3 +301,4 @@ print(sInfo.name,sInfo.ver, "Done.", JSON.stringify({
     Timer.set(1000, false, registerSplitJSON);
   }
 });
+
