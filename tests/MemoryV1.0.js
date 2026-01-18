9cbbcc0e9f378f45b40b4e9d6f68c2ffe8c46b26
Alexander <agabrielsten@gmail.com>
2026-01-18 20:04:49 +0100

    Update MemoryV1.0.js

--- tests/MemoryV1.0.js
+++ tests/MemoryV1.0.js
@@ -5,11 +5,12 @@ Shelly.call("Sys.GetStatus", {}, function (response) {
         let ramSize = response.ram_size || 0;
         let ramFree = response.ram_free || 0;
         let ramUsed = ramSize - ramFree;
-        print("Общий объём памяти: " + ramSize + " байт");
-        print("Свободная память: " + ramFree + " байт");
-        print("Используемая память: " + ramUsed + " байт");
+        print("Total RAM: " + ramSize + " байт");
+        print("Free RAM: " + ramFree + " байт");
+        print("Used RAM: " + ramUsed + " байт");
         print("=======================");
     } else {
-        print("Ошибка получения данных о памяти: " + JSON.stringify(response));
+        print("Error: " + JSON.stringify(response));
     }
+
 });
