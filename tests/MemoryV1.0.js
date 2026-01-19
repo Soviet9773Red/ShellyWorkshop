7c14e719d2f4cebf4bd6a2cb3f200f5cdea3ffca
Alexander <agabrielsten@gmail.com>
2026-01-19 16:54:21 +0100

    Update memory status messages to use 'bites'

--- tests/MemoryV1.0.js
+++ tests/MemoryV1.0.js
@@ -5,12 +5,13 @@ Shelly.call("Sys.GetStatus", {}, function (response) {
         let ramSize = response.ram_size || 0;
         let ramFree = response.ram_free || 0;
         let ramUsed = ramSize - ramFree;
-        print("Total RAM: " + ramSize + " байт");
-        print("Free RAM: " + ramFree + " байт");
-        print("Used RAM: " + ramUsed + " байт");
+        print("Total RAM: " + ramSize + " bites");
+        print("Free RAM: " + ramFree + " bites");
+        print("Used RAM: " + ramUsed + " bites");
         print("=======================");
     } else {
         print("Error: " + JSON.stringify(response));
     }
 
 });
+
